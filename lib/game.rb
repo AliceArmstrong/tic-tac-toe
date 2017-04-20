7f653dd9dfbbd3f8de98c575f95214a0373d25b2
Alice Armstrong <tioma001@hotmail.com>
2017-04-20 15:39:32 +0100

    Initial commit

---
+++ lib/game.rb
@@ -0,0 +1,8 @@
+# game board array
+#
+# current players turn, X ? marker = O : marker = X
+#
+# array.map { [2] == " " ? [2] X : }
+#
+#
+# arr = [" ", " "]
---
+++ spec/game_spec.rb
@@ -0,0 +1,11 @@
+require 'game'
+
+  describe Game do
+
+    it "has a game board" do
+
+    end
+
+
+
+  end
