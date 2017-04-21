49a2fa1a1a32a540f78937133601aad5de38da49
Alice Armstrong <tioma001@hotmail.com>
2017-04-21 11:40:23 +0100

    Implemented switching players

--- lib/game.rb
+++ lib/game.rb
@@ -6,3 +6,16 @@
 #
 #
 # arr = [" ", " "]
+class Game
+  attr_reader :board_arr, :player
+
+  def initialize
+    @board_arr = []
+    @player = "X"
+  end
+
+  def switch_player
+    @player == "X" ? @player = "O" : @player = "X"
+  end
+
+end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,11 +1,20 @@
 require 'game'
 
   describe Game do
+    let(:game) {described_class.new}
+    # game = Game.new
 
     it "has a game board" do
-
+      expect(game.board_arr).to_not be_nil
     end
 
+    it "has a player" do
+      expect(game.player).to eq("X")
+    end
 
+    describe "#switch_player"
+      it "can switch player" do
+        expect(game.switch_player).to eq("O")
+      end
 
   end
